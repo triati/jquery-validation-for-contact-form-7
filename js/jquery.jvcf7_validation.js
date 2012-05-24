7fede2b2c065cf5dfb769a4c7e70d35715cf66ea
dnesscarkey <dnesscarkey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-24 16:02:14 +0000

    git-svn-id: https://plugins.svn.wordpress.org/jquery-validation-for-contact-form-7/trunk@548562 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/jquery.jvcf7_validation.js
@@ -0,0 +1,12 @@
+﻿jQuery(document).ready(function(){
+	jQuery('.wpcf7-validates-as-required').addClass('required');
+	jQuery('.wpcf7-email').addClass('email');
+	
+	jQuery('form.wpcf7-form"').each(function(){
+		jQuery(this).validate({
+			invalidHandler: function(form, validator) {
+				return false;
+			}							   
+		});								
+	});	
+});
